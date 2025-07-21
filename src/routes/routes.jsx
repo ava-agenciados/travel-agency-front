50927a50baee95e89f0008553c64e9fe12cbe525
Rodrigo081pb <kauarodrigo1193@gmail.com>
2025-07-21 20:38:33 -0300

    feature: login page mobile v1 | descrição: implementação da pagina de login já com a api integrada.

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import NotFound from '../pages/NotFound/NotFound.jsx'
 import LandingPage from '../pages/LandingPage/LandingPage.jsx'
+import Login from '../pages/Login/Login.jsx'
 
 const Router = () => {
 
@@ -8,10 +9,15 @@ const Router = () => {
         <>
             <BrowserRouter>
                 <Routes>
+                    
                     <Route path='/' element={<LandingPage/>} />
 
+                    <Route path='/landing' element={<LandingPage/>} />
+
+                    <Route path='/login' element={<Login/>} />
+
                     <Route path='*' element={<NotFound/>} />
-                    
+
                 </Routes>
             </BrowserRouter>
         </>
