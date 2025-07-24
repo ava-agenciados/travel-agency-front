8ea471e9b34581d37d0f6a2c1b67d4366cccc534
devEryckSantos <dev.erycksantos@gmail.com>
2025-07-24 16:18:05 -0300

    Excluindo PlaceHolderImage.

--- src/assets/image.jsx
+++ src/assets/image.jsx
@@ -1,10 +1,5 @@
-import LoginPageImage from './images/LoginPageImage.jpg'
-import PlaceHolderImage from './images/PlaceHolderImage.png'
-
-// Importando imagens da internet (atual)
 const Images = {
     LoginPageImage,
-    PlaceHolderImage
 }
 
 export default Images;
