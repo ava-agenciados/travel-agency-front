cb308a82261939ac6f2a92ccfb1a23a714ef79d7
𝙹𝚊𝚖𝚢𝚕𝚎 𝙴𝚕𝚎𝚗 <110051309+Jamyle-Elen@users.noreply.github.com>
2025-07-25 08:53:42 -0300

    feat: landing page (#7)
    - Menu de navegação com funcionalidade de scroll suave com setas personalizadas
    - Calendario só permite datas de check-in igual ou posterior a data atual e a de check-out só permite igual ou posterior a data do check-in
    - Navbar e Footer componentizados
    - Barra de scroll personalizada na cor azul
    - Resets globais
    
    Co-authored-by: Kauã Rodrigo <147294173+Rodrigo081pb@users.noreply.github.com>

--- src/assets/image.jsx
+++ src/assets/image.jsx
@@ -1,6 +1,12 @@
-import LoginPageImage from "../assets/images/LoginPageImage.jpg"
+import Bg_HeroSection from './images/bg_herosection.png'
+import PalmTree from './images/palm-tree.png'
+import Plan from './images/plan.png'
+import LoginPageImage from './images/LoginPageImage.jpg'
 
 const Images = {
+    Bg_HeroSection,
+    PalmTree,
+    Plan
     LoginPageImage,
 }
 
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,26 @@
+import NavBar from "../../components/Navbar/NavBar";
+import HeroSection from "./components/HeroSection";
+import DayOffers from "./components/DayOffers"
+import BannerPackage from "./components/BannerPackage";
+import DiscountBanner from "./components/DiscountBanner";
+import RegionalDiscountBanner from "./components/RegionalDiscountBanner";
+import Footer from "../../components/Footer/Footer";
+
 const LandingPage = () => {
     return <>
-        <p>Landing Page</p>
+        <NavBar/>
+
+        <HeroSection/>
+
+        <DayOffers/>
+
+        <BannerPackage/>
+
+        <DiscountBanner/>
+
+        <RegionalDiscountBanner/>
+
+        <Footer/>
     </>
 }
 
